0191b66870d7ad3064fbad5f9be27ad43a50ad3e
JoshuaBalles <joshuaballes07@gmail.com>
2024-05-16 03:57:58 +0800

    Update nanaman.

--- app.py
+++ app.py
@@ -5,27 +5,36 @@ from flask import Flask, render_template, jsonify, request, redirect, url_for
 
 app = Flask(__name__)
 
-nodeMCU_ip = '192.168.100.82:80'
-variables = ['temp_val', 'moist_val', 'n_val', 'p_val', 'k_val']
+nodeMCU_ip = "192.168.100.176"
+variables = ["temp_val", "moist_val", "n_val", "p_val", "k_val"]
 last_values = {}
+
 
 def fetch_sensor_values():
     global last_values
     values = {}  # Dictionary to store values
     for variable_name in variables:
-        response = requests.get(f'http://{nodeMCU_ip}/{variable_name}')
-        variable = float(response.text)
-        values[variable_name] = variable
+        try:
+            response = requests.get(f"http://{nodeMCU_ip}/{variable_name}")
+            response.raise_for_status()  # Raise an exception for HTTP errors
+            variable = float(response.text)
+            values[variable_name] = variable
+        except (requests.exceptions.RequestException, ValueError) as e:
+            print(f"Error fetching {variable_name}: {e}")
+            values[variable_name] = last_values.get(variable_name, None)
     last_values = values
     return values
+
 
 @app.route("/")
 def home():
     return render_template("landing.html")
 
+
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
+
 
 @app.route("/parameters", methods=["GET", "POST"])
 def parameters():
@@ -36,13 +45,16 @@ def parameters():
         return redirect(url_for("history"))
     return render_template("parameters.html", values=fetch_sensor_values())
 
+
 @app.route("/history")
 def history():
     return render_template("history.html", values=last_values)
+
 
 @app.route("/get_sensor_values")
 def get_sensor_values():
     return jsonify(fetch_sensor_values())
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
